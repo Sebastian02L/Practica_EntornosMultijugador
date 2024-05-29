43507abe77eea83305be033733a7fd230205a80c
Mdoc14 <marcos.deozaeta@gmail.com>
2024-05-29 17:20:30 +0200

    QUE PRECIOSIDAD DE CODIGO

---
+++ MissileRally/Assets/Scripts/UI_Management/UIStates/MapSelectionState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting.FullSerializer;
+
+internal class MapSelectionState : AUIState
+{
+    public MapSelectionState(IUI UI) : base(UI)
+    {
+    }
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+}
+
