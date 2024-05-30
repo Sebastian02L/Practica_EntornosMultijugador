002c5c4ea0860b702ffed39f7f48907c21010ae6
carlossantayana <carlossantayanavicente@gmail.com>
2024-05-30 23:24:29 +0200

    Nose

--- MissileRally/Assets/Scripts/LapManager.cs
+++ MissileRally/Assets/Scripts/LapManager.cs
@@ -5,10 +5,15 @@ using UnityEngine.InputSystem;
 
 public class LapManager : MonoBehaviour
 {
+    public BoxCollider startLine;
+    public BoxCollider firstCheck;
+    public BoxCollider secondCheck;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstCheck.enabled = false;
+        secondCheck.enabled = false;
     }
 
     // Update is called once per frame
@@ -21,7 +26,25 @@ public class LapManager : MonoBehaviour
     {
         if (other.gameObject.GetComponentInParent<PlayerInput>().enabled == true)
         {
-            GameManager.Instance.player.UpdateCurrentLapServerRpc();
+            if (startLine.enabled)
+            {
+                Debug.Log("Desactivar");
+                startLine.enabled = false;
+                firstCheck.enabled = true;
+                GameManager.Instance.player.UpdateCurrentLapServerRpc();
+            }
+
+            if (firstCheck.enabled)
+            {
+                firstCheck.enabled = false;
+                secondCheck.enabled = true;
+            }
+
+            if (secondCheck.enabled)
+            {
+                secondCheck.enabled = false;
+                startLine.enabled = true;
+            }
         }
     }
 }
