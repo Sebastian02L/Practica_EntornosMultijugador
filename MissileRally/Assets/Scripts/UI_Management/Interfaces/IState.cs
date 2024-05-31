8a9be73f1db936cb6bb7f737e19a555896cb166b
carlossantayana <carlossantayanavicente@gmail.com>
2024-05-31 22:52:39 +0200

    Update IState.cs

--- MissileRally/Assets/Scripts/UI_Management/Interfaces/IState.cs
+++ MissileRally/Assets/Scripts/UI_Management/Interfaces/IState.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Interfaz que define el comportamiento o métodos que todo estado del patrón state debe realizar.
 public interface IState
 {
-    public void Enter();
-    public void Exit();
-    public void Update();
-    public void FixedUpdate();
+    public void Enter(); //Comportamiento al entrar al estado.
+    public void Exit(); //Comportamiento al salir del estado.
+    public void Update(); //Comportamiento del estado en la actualización de paso variable.
+    public void FixedUpdate(); //Comportamiento del estado en la actualización de paso fijo.
 }
