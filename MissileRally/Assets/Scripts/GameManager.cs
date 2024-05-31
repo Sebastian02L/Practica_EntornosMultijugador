7081a5bd14bb88cf63945014b1fdcb94ca5d0fdf
Mdoc14 <marcos.deozaeta@gmail.com>
2024-05-31 22:39:25 +0200

    Gamemanager Comentado

--- MissileRally/Assets/Scripts/GameManager.cs
+++ MissileRally/Assets/Scripts/GameManager.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Player player;
+    public Player player; // Referencia al player que es el owner 
 
-    public ConcurrentDictionary<ulong, PlayerData> players = new ConcurrentDictionary<ulong, PlayerData>();
+    public ConcurrentDictionary<ulong, PlayerData> players = new ConcurrentDictionary<ulong, PlayerData>(); // Diccionario donde se guardan los jugadores por su clientId y que contienen el data de los jugadores
 
-    public bool gameStarted = false;
+    public bool gameStarted = false; // Variable que indica que el la partida ha comenzado
 
-    public int numPlayers = 6;
+    public int numPlayers = 6; // Número máximo de jugadores
 
-    public int playersFinished = 0;
+    public int playersFinished = 0; // Número de jugadores que han terminado la carrera
 
-    public int currentPlayers;
+    public int currentPlayers; // Número de jugadores conectados
 
-    public int readyPlayers;
+    public int readyPlayers; // Número de jugadores listos
 
-    public int mapSelectedId = 0;
+    public int mapSelectedId = 0; // Id del mapa seleccionado: 1-NASCAR 2-OASIS 3-RAINY 4-OWL PLAINS 
 
-    public RaceController currentRace;
+    public RaceController currentRace; // Referencia a la carrera
 
-    public GameObject circuitManager;
+    public GameObject circuitManager; // Referencia al circuit manager
 
     //Variable donde se almacena el valor de la variable de red que lleva el cronometro de la partida
     //Simplemente se hace una asignacion, no se hace nada mas en este script.
