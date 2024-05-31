cfd5d2959b230199434a48eb3a8467ca2553b547
carlossantayana <carlossantayanavicente@gmail.com>
2024-05-31 22:44:48 +0200

    UIManager e IUI

--- MissileRally/Assets/Scripts/UI_Management/Interfaces/IUI.cs
+++ MissileRally/Assets/Scripts/UI_Management/Interfaces/IUI.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Interfaz que define los métodos que debe tener el contexto del patrón state.
+//Como contexto que es del patrón state, se debe poder obtener y establecer el estado. También, como se trata de un state para gestionar la interfaz, se debe poder obtener y 
+//establecer el canvas.
 public interface IUI
 {
     IState State { get; set; }
--- MissileRally/Assets/Scripts/UI_Management/UIManager.cs
+++ MissileRally/Assets/Scripts/UI_Management/UIManager.cs
@@ -10,10 +10,11 @@ using Unity.Services.Relay.Models;
 using Unity.Services.Core;
 using UnityEngine.UI;
 
+//Se realizó un patrón State para gestionar los cambios de interfaz del juego. Esta clase se trata del contexto del patrón.
 public class UIManager : MonoBehaviour, IUI
 {
-    GameObject _canvas;
-    IState _currentState;
+    GameObject _canvas; //Referencia al canvas de la escena para gestionar los cambios de interfaz.
+    IState _currentState; //Referencia al estado actual del contexto.
 
     public IState State
     {
@@ -22,7 +23,7 @@ public class UIManager : MonoBehaviour, IUI
             return _currentState;
         }
 
-        set
+        set //Al establecer un nuevo estado se ejecutado el método de salida del estado anterior y el de entrada al estado siguiente.
         {
             if (_currentState != null)
             {
@@ -48,21 +49,24 @@ public class UIManager : MonoBehaviour, IUI
         }
     }
 
-    const int maxConnections = 5;
+    const int maxConnections = 5; //Número máximo de conexiones de cada cliente sin contarle.
+
+    //Texto por defecto para las áreas de texto en las que introducir el código de sala y el nombre del jugador.
     string joinCode = "Enter room code...";
     public string playerName = "Enter player name...";
 
     private void Start()
     {
         Canvas = GameObject.Find("Canvas");
-        State = new StartingState(this);
+        State = new StartingState(this); //La interfaz se inicializa en el estado de inicio.
     }
 
     private void Update()
     {
-        _currentState.Update();
+        _currentState.Update(); //El contexto ejecuta el método Update de su estado actual.
     }
 
+    //Método encargado de inicializar un runtime como host de una partida del juego.
     async void StartHost()
     {
 
@@ -79,6 +83,7 @@ public class UIManager : MonoBehaviour, IUI
 
     }
 
+    //Método encargado de inicializar un runtime como cliente de una partida de juego que tiene de código "joinCode".
     async void StartClient(string joinCode)
     {
 
@@ -95,15 +100,16 @@ public class UIManager : MonoBehaviour, IUI
 
     }
 
+    //Callback de Unity para el redibujado de la interfaz.
     void OnGUI()
     {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
 
-        if (State is StartingState)
+        if (State is StartingState) //Mientras se esté en el estado inicial, el cual corresponde a no haber seleccionado host o cliente.
         {
-            StartButtons();
+            StartButtons(); //Se muestran los botones de inicio.
         }
-        else
+        else //Para el resto de estados de la interfaz se usará el Canvas y no la función OnGUI, no obstante, se mantienen las etiquetas de estado desde este script.
         {
             StatusLabels();
         }
@@ -111,6 +117,7 @@ public class UIManager : MonoBehaviour, IUI
         GUILayout.EndArea();
     }
 
+    //Método que gestiona la funcionalidad de los botones de inicio para comenzar como host o cliente y las áreas de texto del código de sala y el nombre.
     void StartButtons()
     {
         if (GUILayout.Button("Host")) StartHost();
@@ -119,6 +126,7 @@ public class UIManager : MonoBehaviour, IUI
         joinCode = GUILayout.TextArea(joinCode);
     }
 
+    //Método que muestra etiquetas con el estado de red del juego.
     void StatusLabels()
     {
         var mode = NetworkManager.Singleton.IsHost ?
