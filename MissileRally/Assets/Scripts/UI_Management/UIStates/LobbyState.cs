d2234047c4d4a8c3830b83c33e346742f8ea9e17
Alejandro <alejandro030303@gmail.com>
2024-05-31 23:04:51 +0200

    Update LobbyState.cs

--- MissileRally/Assets/Scripts/UI_Management/UIStates/LobbyState.cs
+++ MissileRally/Assets/Scripts/UI_Management/UIStates/LobbyState.cs
@@ -8,7 +8,7 @@ public class LobbyState : AUIState
     GameObject lobbyInterface;
     TextMeshProUGUI playersCounterText; //Referencia al texto de la interfaz con el numero de jugadores conectados
     TextMeshProUGUI messageStatusText;  //Referencia al texto de la interfaz que indica si se ha seleccionado el mapa y esperando a que los jugadores esten listos
-    TextMeshProUGUI playersReadyText;   //
+    TextMeshProUGUI playersReadyText;   //Referencia al texto de la interfaz que indica el número de jugadores listos del lobby 
     int playersCounter; //Variable que almacena el numero de jugadores conectados
     string messageStatus; //Variable que almacena el estado del Lobby (esperando mapa y que los jugadores esten listos)
     int playersReadyCounter; //
@@ -21,20 +21,21 @@ public class LobbyState : AUIState
 
     public override void Enter()
     {
+        //Se buscan todas las referencias a objetos y se activa la interfaz necesaria
         lobbyInterface = UI.Canvas.transform.Find("LobbyUI").gameObject;
         lobbyInterface.SetActive(true);
         playersCounterText = lobbyInterface.transform.Find("Panel1").Find("Counter").GetComponent<TextMeshProUGUI>();
         messageStatusText = lobbyInterface.transform.Find("Panel2").Find("MessageStatus").GetComponent<TextMeshProUGUI>();
         playersReadyText = lobbyInterface.transform.Find("Panel3").Find("Counter").GetComponent<TextMeshProUGUI>();
 
-        if (GameManager.Instance.mapSelectedId == 0)
+        if (GameManager.Instance.mapSelectedId == 0) //Si el host de la partida na ha seleccionado un mapa todavía, el texto de la interfaz de todos los jugadores cambia
         {
             messageStatus = "Esperando a que el host seleccione un mapa...";
             messageStatusText.text = messageStatus;
         }
     }
 
-    public override void Exit()
+    public override void Exit() //Al salir del estado,se desactiva la interfaz
     {
         lobbyInterface.SetActive(false);
     }
@@ -43,7 +44,7 @@ public class LobbyState : AUIState
     {
     }
 
-    public override void Update()
+    public override void Update() //Se actualizan todos los contadores de la interfaz
     {
         playersCounter = GameManager.Instance.currentPlayers;
         playersReadyCounter = GameManager.Instance.readyPlayers;
@@ -53,7 +54,7 @@ public class LobbyState : AUIState
         //Actualizamos el numero de jugadores listos de la partida
         playersReadyText.text = $"{playersReadyCounter} / {playersCounter}";
 
-        if (!statusChanged && GameManager.Instance.mapSelectedId != 0)
+        if (!statusChanged && GameManager.Instance.mapSelectedId != 0) //Si el host de la partida ya ha seleccionado un mapa, el texto de la interfaz de todos los jugadores cambia
         {
             statusChanged = true;
             messageStatus = "Esperando a que los jugadores estén listos...";
